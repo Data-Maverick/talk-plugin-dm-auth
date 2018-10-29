e45cae67a30b288b4683d517bccb1454fa048e8a
Jason Norwood-Young <jason@10layer.com>
2018-10-29 12:55:55 +0200

    Only show note about only Insiders commenting if you're not an Insider

--- client/stream/components/SignInButton.js
+++ client/stream/components/SignInButton.js
@@ -4,14 +4,13 @@ import styles from './SignInButton.css';
 
 const SignInButton = ({ isLoggedIn, showSignInDialog }) => (
   <div className="talk-stream-auth-sign-in-button">
-    <h4>Only Daily Maverick Insiders can comment. Only subscribers can view comments.</h4>
     {!isLoggedIn ? (
       <a
         href="https://dailymaverick.co.za/insider"
         className={styles.button}
         target="_top"
       >
-        Become an Insider
+        Only Maverick Insiders can comment. Become an Insider
       </a>
     ) : null}
   </div>
