a6238d16c3cc1755cb31e5c6e94658d74d0c14d4
Jason Norwood-Young <jason@10layer.com>
2018-10-29 12:56:08 +0200

    No Comment

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import UserBox from './stream/containers/UserBox';
 import SignInButton from './stream/containers/SignInButton';
 import SetUsernameDialog from './stream/containers/SetUsernameDialog';
+import NoComments from './stream/containers/NoComments';
 import translations from './translations.yml';
 import Login from './login/containers/Main';
 import reducer from './login/reducer';
@@ -9,7 +10,7 @@ export default {
   reducer,
   translations,
   slots: {
-    stream: [UserBox, SignInButton, SetUsernameDialog],
+    stream: [UserBox, SignInButton, SetUsernameDialog, NoComments],
     login: [Login],
   },
 };
