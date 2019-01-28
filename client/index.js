c1c96e56df899e669ea916020812cbaf0b41d414
Jason Norwood-Young <jason@10layer.com>
2019-01-28 12:03:54 +0100

    Try new comment integration

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import UserBox from './stream/containers/UserBox';
 import SignInButton from './stream/containers/SignInButton';
-import ReplyButton from './stream/containers/ReplyButton';
+import Comment from './stream/containers/Comment';
 import SetUsernameDialog from './stream/containers/SetUsernameDialog';
 import translations from './translations.yml';
 import Login from './login/containers/Main';
@@ -10,7 +10,7 @@ export default {
   reducer,
   translations,
   slots: {
-    stream: [UserBox, SignInButton, SetUsernameDialog, ReplyButton],
+    stream: [UserBox, SignInButton, SetUsernameDialog, Comment],
     login: [Login],
   },
 };
